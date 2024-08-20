cb9146607d442c97295ac4b8519ced12dd4e3f29
Gabriel Menezes da Silva <138012112+GMNDS@users.noreply.github.com>
2024-08-20 15:44:03 +0000

    organização de comentários

--- web_i/adivinhacao.bat
+++ web_i/adivinhacao.bat
@@ -1,3 +1,13 @@
+:: Uma variável que armazena o número de tentativas
+:: Gera um número aleatório entre 1 e 50
+:: Uma variável que armazena o número de tentativas restantes
+:: Uma variável gerando numeros randomicos de 0 a 50
+:: Uma variável que armazena o número que o usuário digitou
+:: Verifica se o palpite é igual ao número gerado
+::Pegar o numero que digitamos, comparar com o numero correto
+::se estiver correto, imprime na tela que ele ganhou o jogo e o jogo para 
+:: se estiver errado, imprime na tela que o usuario errou, mostra se o número e maior ou menor e decrementa o numero de tentativas em 1 e retorna para o programa. 
+:: se o numero estiver fora do range especificado pelo programa (0 a 50) retorna que ele precisa digitar um numero dentro do range e retorna ao programa sem diminuir a quantidade de tentativas
 chcp 65001
 @echo off
 title Jogo da adivinhação
@@ -9,17 +19,7 @@ echo ╔════════════════════════
 echo ║██ Jogo de adivinhação em bat ██║
 echo ╚════════════════════════════════╝
 echo.
-:: Uma variável que armazena o número de tentativas
-:: Gera um número aleatório entre 1 e 50
-:: Uma variável que armazena o número de tentativas restantes
-:: Uma variável gerando numeros randomicos de 0 a 50
-:: Uma variável que armazena o número que o usuário digitou
 set /p palpite=Digite um número entre 1 e 50:
-:: Verifica se o palpite é igual ao número gerado
-::Pegar o numero que digitamos, comparar com o numero correto
-::se estiver correto, imprime na tela que ele ganhou o jogo e o jogo para 
-:: se estiver errado, imprime na tela que o usuario errou, mostra se o número e maior ou menor e decrementa o numero de tentativas em 1 e retorna para o programa. 
-:: se o numero estiver fora do range especificado pelo programa (0 a 50) retorna que ele precisa digitar um numero dentro do range e retorna ao programa sem diminuir a quantidade de tentativas
 echo Você tem %tentativas% tentativas
 if %palpite% == %numero% (
     echo Parabéns! você acertou!
