1d59be725e508749eab814a0fea4b7373e1460b4
GMNDS <gabriel@gmnds.com>
2024-08-23 16:58:15 -0300

    atualizacao web_i

--- web_i/adivinhacao.bat
+++ web_i/adivinhacao.bat
@@ -8,21 +8,65 @@
 ::se estiver correto, imprime na tela que ele ganhou o jogo e o jogo para 
 :: se estiver errado, imprime na tela que o usuario errou, mostra se o número e maior ou menor e decrementa o numero de tentativas em 1 e retorna para o programa. 
 :: se o numero estiver fora do range especificado pelo programa (0 a 50) retorna que ele precisa digitar um numero dentro do range e retorna ao programa sem diminuir a quantidade de tentativas
-chcp 65001
 @echo off
+chcp 65001
 title Jogo da adivinhação
-color 05
+color 03
+cls
 set /a numero=(%random% %% 50) + 1
 set /a tentativas=5
 :inicio
 echo ╔════════════════════════════════╗
 echo ║██ Jogo de adivinhação em bat ██║
 echo ╚════════════════════════════════╝
+echo ------------------------------------------------
+echo Tentativas: %tentativas% %numero%
+echo ------------------------------------------------
 echo.
-set /p palpite=Digite um número entre 1 e 50:
-echo Você tem %tentativas% tentativas
+set /p palpite=Digite um número entre 1 e 50: 
+
+for /f "delims=0123456789" %%i in ("%palpite%") do (
+    cls
+    color 0C
+    echo Você precisa digitar um número entre 1 e 50
+    echo.
+    pause
+    cls
+    color 03
+    goto inicio
+)
+
+if %palpite% LSS 1 (
+    cls
+    color 0C
+    echo Você precisa digitar um número entre 1 e 50
+    echo.
+    pause
+    cls
+    color 03
+    goto inicio
+)
+if %palpite% GTR 50 (
+    cls
+    color 0C
+    echo Você precisa digitar um número entre 1 e 50
+    echo.
+    pause
+    cls
+    color 03
+    goto inicio
+)
+
 if %palpite% == %numero% (
-    echo Parabéns! você acertou!
+    cls
+    echo     ▄   ████▄ ▄█▄    ▄███▄         ▄▀  ██      ▄    ▄  █ ████▄   ▄   
+    echo      █  █   █ █▀ ▀▄  █▀   ▀      ▄▀    █ █      █  █   █ █   █    █  
+    echo █     █ █   █ █   ▀  ██▄▄        █ ▀▄  █▄▄█ ██   █ ██▀▀█ █   █ █   █ 
+    echo  █    █ ▀████ █▄  ▄▀ █▄   ▄▀     █   █ █  █ █ █  █ █   █ ▀████ █   █ 
+    echo   █  █        ▀███▀  ▀███▀        ███     █ █  █ █    █        █▄ ▄█ 
+    echo    █▐                                    █  █   ██   ▀          ▀▀▀  
+    echo    ▐                                    ▀                                                                                                    
+    echo.
     pause
     exit
 ) else (
@@ -32,16 +76,20 @@ if %palpite% == %numero% (
         pause
         exit
     ) 
-    echo Você errou meu parceiro!
-    if %palpite% LSS %numero% (
-        echo O número é menor do que seu palpite 
+    echo. 
+    if %numero% LSS %palpite% (
+        echo Você errou! O número é menor do que seu palpite 
+        echo.
     )
-    if %palpite% GTR %numero% (
-        echo O numero é maior do que seu palepite
+    if %numero% GTR %palpite% (
+        echo Você errou! O numero é maior do que seu palpite
+        echo.
     )
-    %tentativas% = %tentativas% - 1
+    
+    set /a tentativas = %tentativas% - 1
+
+    pause
+    cls
+    goto inicio
 )
 pause
-
-@REM echo Você tem restatantes %restantes% tentativas
-
--- web_i/bash.md
+++ web_i/bash.md
@@ -1,47 +1,25 @@
-:: Uma variável que armazena o número de tentativas
-:: Gera um número aleatório entre 1 e 50
-:: Uma variável que armazena o número de tentativas restantes
-:: Uma variável gerando numeros randomicos de 0 a 50
-:: Uma variável que armazena o número que o usuário digitou
-:: Verifica se o palpite é igual ao número gerado
-::Pegar o numero que digitamos, comparar com o numero correto
-::se estiver correto, imprime na tela que ele ganhou o jogo e o jogo para 
-:: se estiver errado, imprime na tela que o usuario errou, mostra se o número e maior ou menor e decrementa o numero de tentativas em 1 e retorna para o programa. 
-:: se o numero estiver fora do range especificado pelo programa (0 a 50) retorna que ele precisa digitar um numero dentro do range e retorna ao programa sem diminuir a quantidade de tentativas
-chcp 65001
-@echo off
-title Jogo da adivinhação
-color 05
-set /a numero=(%random% %% 50) + 1
-set /a tentativas=5
-:inicio
-echo ╔════════════════════════════════╗
-echo ║██ Jogo de adivinhação em bat ██║
-echo ╚════════════════════════════════╝
-echo.
-set /p palpite=Digite um número entre 1 e 50:
-echo Você tem %tentativas% tentativas
-if %palpite% == %numero% (
-    echo Parabéns! você acertou!
-    pause
-    exit
-) else (
-    if %tentativas%==0 (
-        echo Você perdeu
-        echo O número correto é %numero%
-        pause
-        exit
-    ) 
-    echo Você errou meu parceiro!
-    if %palpite% LSS %numero% (
-        echo O número é menor do que seu palpite 
-    )
-    if %palpite% GTR %numero% (
-        echo O numero é maior do que seu palepite
-    )
-    %tentativas% = %tentativas% - 1
-)
-pause
+# Bash
 
-@REM echo Você tem restatantes %restantes% tentativas
+## Operadores lógicos
 
+- Igual: `Equal` ou `==`
+- Diferente: `NEQ`
+- Menor que: `LSS`
+- Menor ou igual a: `LEQ`
+- Maior que: `GTR`
+- Maior ou igual a: `GEQ`
+- Condição negativa: `NOT`
+- Existe: `EXIST`
+- Contador `set /a cont=%cont% + 1`
+- Números randômicos:`set /a numero=(%Random% %% 10) + 1`
+- Estrutura de repetição: `/L %%n in (1,1,4) do (Estrutura de repetição)`
+
+## Joken Po
+
+- Digitar o nome
+- Pedra, Papel, Tesoura, Lagarto, Spock
+- Pedra: Empata com Pedra; Ganha de Tesoura e Lagarto; Perde de Papel e Spock;
+- Papel: Empata com Papel; Ganha de Pedra e Spock; Perde de Tesoura e Lagarto;
+- Tesoura: Empata com Tesoura; Ganha de Lagarto e Papel; perde de pedra e Spock;
+- Lagarto: Empata com Lagarto; Ganha de Papel e Spock; Perde de Pedra e Tesoura;
+- Spock: Empata com Spock; Ganha de Pedra e Tesoura; Perd de Lagarto e Papel;
